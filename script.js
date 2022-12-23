65f0e43cf94887ff4718affeaa6c9b7e9c21a2f7
devJuliaLeal <juliaassuncaoleal@gmail.com>
2022-12-23 09:20:21 -0300

    commit

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operator]');
-const equalsButtons = document.querySelector('[data-equals]');
-const deletesButtons = document.querySelector('[data-delete]');
-const previousOperandTextElement = document.querySelector('[data-previous-operand]');
-const currentOperandTextElement = document.querySelector('[data-current-operand]');
+const numberButtons = document.querySelectAll("[data-number]");
+const operationButtons = document.querySelectAll("[data-operator]");
+const deleteButtons = document.querySelectAll("[data-delete]");
+const previousOperandTextElement = document.querySelectAll(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelectAll(
+  "[data-current-operand]"
+);
+const allClearButtons = document.querySelectAll("[data-all-clear]");
 
-class Calculator 
-{
-    constructor (previousOperandTextElement,currentOperandTextElement)
-    {
-        this.previousOperandTextElement = previousOperandTextElement;
-        this.currentOperandTextElement = currentOperandTextElement; 
-        clear()
-        this.currentOperand ='';
-        this.previousOperand ='';
-        this.operation = undefined; 
-    }
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+
+  updateDisplay() {
+    this.previousOperandTextElement.innerText = this.previousOperand;
+    this.currentOperandTextElement.innerText = this.currentOperand;
+  }
 }
-const Calculator =new Calculator
-(
-    previousOperandTextElement,
-    currentOperandTextElement
-)
-allClearButton.addEventListener('click', ()) 
+
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+allClearButtons.addEventListener("click", () => {
+  Calculator.clear();
+  Calculator.updateDisplay();
+});
